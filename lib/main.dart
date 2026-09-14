89f9f21b2449d4a9f57d90f670823f93a54826eb
Rehamalkhamees <rahommy11@gmail.com>
2026-09-14 17:33:42 +0300

    apdate two pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:space_rental/screens/auth/welcome_page.dart';
+import 'package:space_rental/screens/auth/signup_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/auth/welcome_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://yricmfxnruipcgfrmozq.supabase.co',
+    anonKey: 'sb_publishable_2B4zCj5Eef11BzV7VCPGfw_19OsV4Lb',
+  );
+
+  runApp(const SpaceOraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SpaceOraApp extends StatelessWidget {
+  const SpaceOraApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      title: 'SpaceOra',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const SignupPage(),
     );
   }
 }
